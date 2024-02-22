61f028db9a863eb66a5f51dc571ee5ac9aaffb4d
Akiulah <akilah.yu@gmail.com>
2024-02-21 20:05:01 -0500

    khgjghm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServosCode.java
@@ -0,0 +1,11 @@
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.Servo;
+//
+//@TeleOp
+//public class ServosCode extends OpMode {
+//    public Servo Launcher Servo;
+//
+//}
